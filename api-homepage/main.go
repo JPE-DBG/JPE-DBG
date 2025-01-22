618e2fe01395c21c0e14f9094a5f88864733359b
Jiri Pesek <jiri.pesek@deutsche-boerse.com>
2025-01-22 19:46:31 +0100

    Update main.go

--- api-homepage/main.go
+++ api-homepage/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
 
 func main() {
-	os.Exit(0)
+	http.HandleFunc("/", mainHandler)
+	fmt.Println("Starting server on :10000")
+	if err := http.ListenAndServe(":10000", nil); err != nil {
+		fmt.Println("Server failed to start:", err)
+		os.Exit(1)
+	}
 }
